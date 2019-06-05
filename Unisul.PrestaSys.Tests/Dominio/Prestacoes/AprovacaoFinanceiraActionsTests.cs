c30cd30443573837009bd6a9105382e29952157e
F, Cristian - Dell Team <cristian_f@dellteam.com>
2019-06-05 10:05:32 -0300

    Adicionados mais testes

--- Unisul.PrestaSys.Tests/Dominio/Prestacoes/AprovacaoFinanceiraActionsTests.cs
+++ Unisul.PrestaSys.Tests/Dominio/Prestacoes/AprovacaoFinanceiraActionsTests.cs
@@ -52,6 +52,28 @@ namespace Unisul.PrestaSys.Tests.Dominio.Prestacoes
             // Assert
             prestacao.StatusId.Should().Be((int)PrestacaoStatuses.Rejeitada);
             prestacao.JustificativaAprovacaoFinanceira.Should().Be(justificativa);
-        }   
+        }
+
+        [TestMethod]
+        public void GetEmailToShouldReturnAprovadorFinanceiroId()
+        {
+            // Arrange
+            var prestacaoRepository = Mock.Of<IPrestacaoRepository>();
+            var usuarioService = Mock.Of<IUsuarioService>();
+
+            var prestacao = new Prestacao {Id = 1, Titulo = "Teste", AprovadorFinanceiroId = 2};
+            var expectedUsuario = "test@test.com";
+
+            Mock.Get(usuarioService).Setup(x => x.GetUsuarioEmailById(prestacao.AprovadorFinanceiroId)).Returns(expectedUsuario);
+
+            var rejeitadaActions =
+                new AprovacaoFinanceiraActions(prestacaoRepository, usuarioService);
+
+            // Act
+            var returnedUsuario = rejeitadaActions.GetEmailTo(prestacao);
+
+            // Assert
+            returnedUsuario.Should().Be(expectedUsuario);
+        }
     }
 }
---
+++ Unisul.PrestaSys.Tests/Dominio/Prestacoes/FinalizadaActionsTests.cs
@@ -0,0 +1,88 @@
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using Unisul.PrestaSys.Dominio.Servicos.Prestacoes.PrestacaoStatusActions;
+using Unisul.PrestaSys.Dominio.Servicos.Usuarios;
+using Unisul.PrestaSys.Entidades.Prestacoes;
+using Unisul.PrestaSys.Repositorio.Prestacoes;
+
+namespace Unisul.PrestaSys.Tests.Dominio.Prestacoes
+{
+    [TestClass]
+    public class FinalizadaActionsTests
+    {
+        [TestMethod]
+        public void AprovarPrestacaoShouldThrowNotSupportedException()
+        {
+            // Arrange
+            var prestacaoRepository = Mock.Of<IPrestacaoRepository>();
+            var usuarioService = Mock.Of<IUsuarioService>();
+
+            var prestacao = new Prestacao {Id = 1, Titulo = "Teste"};
+            const string justificativa = "Teste de Justificativa";
+
+            var rejeitadaActions =
+                new FinalizadaActions(prestacaoRepository, usuarioService);
+
+            // Act & Assert
+            Assert.ThrowsException<NotSupportedException>(
+                () => rejeitadaActions.AprovarPrestacao(prestacao, justificativa)); 
+        }
+
+        [TestMethod]
+        public void RejeitarPrestacaoShouldThrowNotSupportedException()
+        {
+            // Arrange
+            var prestacaoRepository = Mock.Of<IPrestacaoRepository>();
+            var usuarioService = Mock.Of<IUsuarioService>();
+
+            var prestacao = new Prestacao {Id = 1, Titulo = "Teste"};
+            const string justificativa = "Teste de Justificativa";
+
+            var rejeitadaActions =
+                new FinalizadaActions(prestacaoRepository, usuarioService);
+
+            // Act & Assert
+            Assert.ThrowsException<NotSupportedException>(
+                () => rejeitadaActions.RejeitarPrestacao(prestacao, justificativa)); 
+        }
+
+        [TestMethod]
+        public void GetAllParaAprovacaoShouldThrowNotSupportedException()
+        {
+            // Arrange
+            var prestacaoRepository = Mock.Of<IPrestacaoRepository>();
+            var usuarioService = Mock.Of<IUsuarioService>();
+
+            var rejeitadaActions =
+                new FinalizadaActions(prestacaoRepository, usuarioService);
+
+            // Act & Assert
+            Assert.ThrowsException<NotSupportedException>(
+                () => rejeitadaActions.GetAllParaAprovacao(1)); 
+        }
+
+        [TestMethod]
+        public void GetEmailToShouldReturnEmitentId()
+        {
+            // Arrange
+            var prestacaoRepository = Mock.Of<IPrestacaoRepository>();
+            var usuarioService = Mock.Of<IUsuarioService>();
+
+            var prestacao = new Prestacao {Id = 1, Titulo = "Teste", EmitenteId = 2};
+            var expectedUsuario = "test@test.com";
+
+            Mock.Get(usuarioService).Setup(x => x.GetUsuarioEmailById(prestacao.EmitenteId)).Returns(expectedUsuario);
+
+            var finalizadaActions =
+                new FinalizadaActions(prestacaoRepository, usuarioService);
+
+            // Act
+            var returnedUsuario = finalizadaActions.GetEmailTo(prestacao);
+
+            // Assert
+            returnedUsuario.Should().Be(expectedUsuario);
+        }
+    }
+}
